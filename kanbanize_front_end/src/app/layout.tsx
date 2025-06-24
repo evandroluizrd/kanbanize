1bf5f7ec26e3731041b10d6a6c3e10c760da7d88
Gustavo Valsechi <gu_vf@hotmail.com>
2025-06-23 21:14:37 -0300

    fix

--- kanbanize_front_end/src/app/layout.tsx
+++ kanbanize_front_end/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Providers } from "@/contexts";
+import Header from "../components/Header"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,12 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-          {children}
-        </Providers>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
+        {children}
       </body>
     </html>
   );
